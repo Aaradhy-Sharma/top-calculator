35812b56853b6da4da682a2a808558cd8588f854
Aaradhy Sharma <as783@snu.edu.in>
2023-07-22 18:16:24 +0530

    jsUpdate: added basic enter-clear functionality. Further updates pending.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log('initial commit'); 
+const numberButtons = document.querySelectorAll('[data-numbers]');
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => handleNumbers(button.textContent));
+});
+
+const operatorButtons = document.querySelectorAll('[data-operators]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+let previousOperand = '';
+let currentOperand = '';
+let operator = '';
+
+function handleNumbers(num) {
+    currentOperand += num;
+    currentOperandTextElement.textContent = currentOperand;
+    console.log('Current Operand:', currentOperand);
+}
+
+function clear() {
+    currentOperand = '';
+    previousOperand = '';
+    operator = '';
+    currentOperandTextElement.textContent = '';
+    previousOperandTextElement.textContent = '';
+}
+clearButton.addEventListener('click', clear);
+
+
+console.log('Script Loaded Successfully');
